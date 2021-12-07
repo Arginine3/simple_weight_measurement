73be7d9b87cee5548d82545bfa4c28ec93f794f5
Arginine3 <ym.love.1003@icloud.com>
2021-12-08 01:09:05 +0900

    リレーションの設定
    親、子ともにリレーションの設定を行った後に fillも設定した。

--- src/app/Models/PersonalInfo.php
+++ src/app/Models/PersonalInfo.php
@@ -4,7 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+//use でWeightMonthModelを読み込む必要があるかも
+
 class PersonalInfo extends Model
 {
-    //
+    /**
+    * 複数代入する属性
+    *
+    * @var array
+    */
+    protected $fillable = [
+        'clint_name',
+        'birth_date',
+        'sex',
+        'height'
+    ];
+
+    //「１対多」の「多」側 → メソッド名は複数形
+    public function WeightMonths()
+    {
+        //リレーションの設定
+        return $this->hasMany('App\Models\WeightMonth', 'client_id', 'id');
+    }
 }
--- src/app/Models/WeightMonth.php
+++ src/app/Models/WeightMonth.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class WeightMonth extends Model
 {
-    //
+    /**
+    * 複数代入する属性
+    *
+    * @var array
+    */
+    protected $fillable = [
+        'client_id',
+        'year_month_date',
+        'weight'
+    ];
+    
+    //「１対多」の「多」側 → メソッド名は複数形
+    public function PersonalInfo()
+    {
+        //リレーションの設定
+        return $this->belongsTo('App\Models\PersonalInfo', 'client_id');
+    }
 }
