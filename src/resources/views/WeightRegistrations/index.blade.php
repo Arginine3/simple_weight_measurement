2fcba59b8bb6ca6bdd0d040b3c93d30df114afb6
Arginine3 <ym.love.1003@icloud.com>
2021-12-10 00:13:49 +0900

    列の値と表示される値を変更
    home画面で表示される内容が最適でないとおもったので変更したので記録

--- src/resources/views/WeightRegistrations/index.blade.php
+++ src/resources/views/WeightRegistrations/index.blade.php
@@ -19,8 +19,9 @@
                             <tr>
                                 <th scope="col">id</th>
                                 <th scope="col">氏名</th>
-                                <th scope="col">性別(year_month_dateをいれる)</th>
-                                <th scope="col">詳細</th>
+                                <th scope="col">性別</th>
+                                <th scope="col">生年月日</th>
+                                <th scope="col">client status</th>
                                 <th scope="col">削除</th>
                             </tr>
                         </thead>
@@ -30,7 +31,8 @@
                                     <th>{{$personal_info->id }}</th>
                                     <td>{{$personal_info->clint_name}}</td>
                                     <td>{{$personal_info->sex}}</td>
-                                    <td><a href="{{route('WeightRegistrations.show', ['id' => $personal_info->id])}}">詳細を見る</a></td>
+                                    <td>{{$personal_info->birth_date}}</td>
+                                    <td><a href="{{route('WeightRegistrations.show', ['id' => $personal_info->id])}}">詳細</a></td>
                                     <td><form method="POST" action="{{route('WeightRegistrations.destroy', ['id' => $personal_info->id])}}" id="delete_{{$personal_info->id}}">
                                             @csrf
                                             <a href="#" style="color: red;" data-id="{{$personal_info->id}}" onclick="deletePost(this);">削除する</a>
