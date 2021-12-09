69e32116a2a84af4ebab8ddd40ca6b0e3ccbcea8
Arginine3 <ym.love.1003@icloud.com>
2021-12-09 15:31:50 +0900

    リレーションしているテーブルにフォームの入力内容を保存する昨日を追加
    2つのテーブルをリレーションさせてデータをほぞんできるようになったので記録

--- src/resources/views/WeightRegistrations/confirm.blade.php
+++ src/resources/views/WeightRegistrations/confirm.blade.php
@@ -16,19 +16,23 @@
                     こちらの内容で登録します。よろしいですか??<br>
                     <form method="post" action="{{route('WeightRegistrations.store')}}">
                     @csrf
+
                         <div>{{ $personal_infos["clint_name"] }}</div>
                         <div>{{ $personal_infos['birth_date'] }}</div>
                         <div>{{ $personal_infos['sex'] }}</div>
                         <div>{{ $personal_infos['height'] }}</div>
                         <div>{{ $weight_months['year_month_date'] }}</div>
                         <div>{{ $weight_months['weight'] }}</div>
-                        <input name="clint_name" type="hidden" value="{{ $personal_infos["clint_name"] }}">
-                        <input name="birth_date" type="hidden" value="{{ $personal_infos["birth_date"] }}">
-                        <input name="sex" type="hidden" value="{{ $personal_infos["sex"] }}">
-                        <input name="height" type="hidden" value="{{ $personal_infos["height"] }}">
-                        <input name="client_id" type="hidden" value="{{ $weight_months["client_id"] }}">
-                        <input name="year_month_date" type="hidden" value="{{ $weight_months["year_month_date"] }}">
-                        <input name="weight" type="hidden" value="{{ $weight_months["weight"] }}">
+
+                        <!-- DBにデータを保存するために リクエストの形で postメソッドに送る -->
+                        <input name="clint_name" type="hidden" value="{{ $personal_infos->clint_name }}">
+                        <input name="birth_date" type="hidden" value="{{ $personal_infos->birth_date }}">
+                        <input name="sex" type="hidden" value="{{ $personal_infos->sex }}">
+                        <input name="height" type="hidden" value="{{ $personal_infos->height }}">
+                        <!-- <input name="client_id" type="hidden" value=""> -->
+                        <input name="year_month_date" type="hidden" value="{{ $weight_months->year_month_date }}">
+                        <input name="weight" type="hidden" value="{{ $weight_months->weight }}">
+
                         <button class="btn btn-warning" name="back" type="submit" value="true">戻る</button>
                         <button class="btn btn-primary" name="" type="submit" value="false">登録</button>
                     </form>
