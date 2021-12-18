c60c5c1eb51ed36565c08485ff8408e4106600d5
Arginine3 <ym.love.1003@icloud.com>
2021-12-18 23:35:04 +0900

    ymlファイルの作成

---
+++ src/tests/Feature/WeightRegistrationControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class WeightRegistrationControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testIndex()
+    {
+        $response = $this->get(route('WeightRegistrations.index'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('WeightRegistrations.index');
+    }
+}
