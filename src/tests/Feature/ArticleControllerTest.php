4f554f4eca95f7ab1bdfa424983ca7893dcd03f4
Arginine3 <ym.love.1003@icloud.com>
2021-12-20 13:34:42 +0900

    テスト4

--- src/tests/Feature/ArticleControllerTest.php
+++ src/tests/Feature/ArticleControllerTest.php
@@ -6,18 +6,11 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class WeightRegistrationControllerTest extends TestCase
+class ArticleControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    // public function testIndex()
-    // {
-    //     $response = $this->get(route('WeightRegistrations.index'));
-
-    //     $response->assertStatus(200)
-    //         ->assertViewIs('WeightRegistrations.index');
-    // }
-        public function testCreate()
+    public function testCreate()
     {
         $response = $this->get(route('WeightRegistrations.create'));
 
