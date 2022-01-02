bd875c12330ba7c22c09f2122901dd573927f2c1
Arginine3 <ym.love.1003@icloud.com>
2022-01-02 23:50:28 +0900

    Seederのデータを追加

--- src/resources/views/WeightRegistrations/index.blade.php
+++ src/resources/views/WeightRegistrations/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">index.blade.php</div>
+                <div class="card-header">一覧画面</div>
 
                 <div class="card-body">
                     <!-- @if (session('status'))
