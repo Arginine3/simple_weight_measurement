1d1abf72bfaac86b3bf87fa8da6bb1270f99008b
Arginine3 <ym.love.1003@icloud.com>
2022-01-06 17:35:56 +0900

    個人情報詳細画面の文字表示を変更

--- src/resources/views/WeightRegistrations/show.blade.php
+++ src/resources/views/WeightRegistrations/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">show.blade.php</div>
+                <div class="card-header">個人情報編集画面</div>
 
                 <div class="card-body">
                     <!-- @if (session('status'))
