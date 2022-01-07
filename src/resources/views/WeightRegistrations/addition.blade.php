54a40b8cc6dc23d294bf0a5e5d92ac75a9c3661f
Arginine3 <ym.love.1003@icloud.com>
2022-01-07 21:29:29 +0900

    今月の体重登録画面の表示を変更

--- src/resources/views/WeightRegistrations/addition.blade.php
+++ src/resources/views/WeightRegistrations/addition.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">addition.blade.php</div>
+                <div class="card-header">今月の体重登録画面</div>
 
                 <div class="card-body">
                     <!-- @if (session('status'))
