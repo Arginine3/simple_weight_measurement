88acee23da1f139f7176dd4244ef67ec021379bb
Arginine3 <ym.love.1003@icloud.com>
2022-02-01 23:21:58 +0900

    リファクタリング

--- src/app/Http/Controllers/WeightRegistrationController.php
+++ src/app/Http/Controllers/WeightRegistrationController.php
@@ -161,7 +161,7 @@ class WeightRegistrationController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * 
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
