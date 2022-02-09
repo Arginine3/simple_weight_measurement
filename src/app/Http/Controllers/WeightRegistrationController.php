d88c032feaf69c87cc0255578f2e2e88d07de47b
Arginine3 <ym.love.1003@icloud.com>
2022-02-09 20:12:14 +0900

    無駄コード削除

--- src/app/Http/Controllers/WeightRegistrationController.php
+++ src/app/Http/Controllers/WeightRegistrationController.php
@@ -147,7 +147,6 @@ class WeightRegistrationController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
